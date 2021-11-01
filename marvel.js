271ea8f4e1f856aeb4e51ad5e2c51e35563ed09b
NL <Lapina.n.b@mail.ru>
2021-11-02 03:07:53 +1000

    js

--- marvel.js
+++ marvel.js
@@ -4,42 +4,46 @@ const heros = [
     universe: 'DC Comics',
     frend: 'собака Крипто',
     photo: '',
-    rating: true
+    rating : 'dislike',
+    rating : 'norm',
+    rating : 'like'
 },{
     name: 'Дэдпул',
     universe: 'Marvel Comics',
     frend: 'частично Мстители, Человек-паук, Росомаха',
     photo: '',
-    rating: false
+    rating : 'dislike',
+    rating : 'norm',
+    rating : 'like'
 },{
     name: 'Железный человек',
     universe: 'Marvel Comics',
     frend: 'Мстители',
     photo: '',
-    rating: false
+    rating : 'dislike',
+    rating : 'norm',
+    rating : 'like'
 }
 ];
 
 let result = "";
 
+
+
 for (hero of heros){
     result += 
-    
     '<div class="hero_name">'+ 'Герой: ' + hero.name + '</div>' +
     '<div class="hero_universe">' + 'Вселенная: ' + hero.universe + '</div>'+ 
     '<div class="hero_frend">' + 'Друзья: ' + hero.frend + '</div>'+ 
     '<div class="hero_photo">' + '' + hero.photo + '</div>'+ 
         '<p>Поставти пожалуйста рейтинг</p>'+
         '<div>'+
-            '<input type="radio" name="rating" value="'+ 
-            hero.rating + '">'+ 
-            '<label>Не нравиться герой</label>'+
-
-            '<input type="radio" name="rating" value="">'+
-            '<label>Нормальный</label>'+
-
-            '<input type="radio" name="rating" value="" >'+
-            '<label">Любимый герой</label>'+
+        '<input type="radio" name="rating" value="dislike" ' + (hero.rating === 'dislike' ? 'selected' : '' ) + 
+        '><label>Не нравиться герой</label>' + 
+        '<input type="radio" name="rating" value="norm" ' + (hero.rating === 'norm' ? 'selected' : '' ) + 
+        '><label>Нормально</label>' + 
+        '<input type="radio" name="rating" value="like" ' + (hero.rating === 'like' ? 'selected' : '' ) + 
+        '><label>Любимый герой</label>'+
             '<br>'+        
             '<button type="submit">Проголосовать</button>'+
         '</div>'+
